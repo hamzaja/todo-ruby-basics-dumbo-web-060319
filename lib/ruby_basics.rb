d44e68672093380888fb4a25bd7fe6950257ba63
HAMZA Javed <hamzajaved71@gmail.com>
2019-05-18 21:12:12 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
 def division(num1, num2)
- 
+  return (num2/num1)
 end
 
 def assign_variable(value)
+  value
   
 end
 
-def argue
+def argue(num)
+  num
   
 end
 
-def greeting
+def greeting(greet , name)
 
 end
 
 def return_a_value
+  nice
 
 end
 
 def last_evaluated_value
+  return "expert"
 
 end
 
-def pizza_party
-
+def pizza_party(num = "cheese")
+num
 end
